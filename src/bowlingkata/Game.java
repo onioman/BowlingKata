fb214bf372460fb6c3e85eb83db206ec028f02e1
Antonio F. Lancho <onioman@gmail.com>
2011-10-20 22:55:24 +0900

    passes now oneSpareInGame

---
+++ src/bowlingkata/Game.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowlingkata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Game {
+    
+    private class Frame {
+        
+        private List<Integer> rolls;
+        private int pins;
+        
+        public Frame() {
+            rolls   = new ArrayList<Integer>();
+            pins    = 0;
+        }
+        
+        public void roll(int p) {
+            rolls.add(p);
+            pins += p;
+        }
+        
+        private boolean isStrike(){
+            return (pins == 10 && rolls.size() == 1);
+        }
+        
+        private boolean isSpare(){
+            return (pins == 10 && rolls.size() == 2);
+        }
+                
+        private int singleBonus() {
+            return rolls.get(0);
+        }
+        
+        private int doubleBonus( Frame next ) {
+            if (next==null) return pins;
+            if (this.isStrike())
+                return ( pins + next.singleBonus());
+            else
+                return pins;
+        }
+        
+        private int bonus(Frame next, Frame nextNext) {
+            if ( next == null ) return 0;
+            if (this.isStrike()) {
+                return next.doubleBonus(nextNext);
+            }
+            else if (this.isSpare()) {
+                return next.singleBonus();
+            } else {
+                return 0;
+            }
+        }
+        
+        public int score( Frame next, Frame nextNext ) {
+            return (pins + this.bonus(next, nextNext));
+        }
+        
+        public boolean isComplete() {
+            return ( this.isStrike() ||
+                     rolls.size() == 2 );
+        }
+    }
+    
+    private Frame[] frames;
+    private int   index;
+
+    public Game() {        
+        frames = new Frame[10];
+        for(int i=0;i<10;i++)
+            frames[i] = new Frame();
+        index = 0;
+    }
+    
+    private Frame curFrame(){
+        return frames[index];
+    }
+
+    public void roll(int pins) {        
+        if (curFrame().isComplete()) {
+            index++;
+        }        
+        curFrame().roll(pins);
+    }
+
+    public int score(){
+        //return points;
+        int total = 0;
+        for(int i=0; i<8; i++){
+            total += frames[i].score(frames[i+1], frames[i+2]);
+        }
+        total += frames[8].score(frames[9],null);
+        total += frames[9].score(null, null);
+        return total;
+    }
+
+}
---
+++ test/bowlingkata/GameTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowlingkata;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author onio
+ */
+public class GameTest {
+    private Game instance;
+
+    public void rollMany(final int times, final int pins) {
+        for (int roll=0; roll<times; roll++)
+            instance.roll(pins);
+    }
+
+    public void rollStrike() {
+        instance.roll(10);
+    }
+
+    @Before
+    public void setUp() {
+        instance = new Game();
+    }
+
+    @Test
+    public void loserGame() {
+        rollMany(20, 0);
+        assertEquals(0, instance.score());
+    }
+
+    @Test
+    public void onePinPerRollGame() {
+        rollMany(20, 1);
+        assertEquals(20, instance.score());
+    }
+
+    @Test
+    public void oneStrikeInGame() {
+        rollStrike();
+        rollMany(18, 1);
+        assertEquals(10 + 2 + 18, instance.score());
+    }
+
+    @Test
+    public void twoChainedStrikesInGame() {
+        rollStrike();
+        rollStrike();
+        rollMany(16, 1);
+        assertEquals(
+                10 + 10 + 1 +
+                10 +  1 + 1 +
+                16, instance.score());
+    }
+
+    @Test
+    public void oneSpareInGame() {
+        instance.roll(5);
+        instance.roll(5);
+        rollMany(18, 1);
+        assertEquals(10 + 1 + 18, instance.score());
+    }
+
+//    @Test
+//    public void perfectGame() {
+//        rollMany(10+2, 10);
+//        assertEquals(300, instance.score());
+//    }
+}
